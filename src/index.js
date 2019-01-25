a7e936fc1c8cd52b5a44d2688edad57a062fabce
nicolaserny <nicolas.erny@xcomponent.com>
2019-01-25 18:14:34 +0100

    Update to react-gojs 3.0.0

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as go from 'gojs';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const gojsKey = process.env.REACT_APP_GOJS_KEY;
+
+if (gojsKey) {
+    // tslint:disable-next-line:no-any
+    go.licenseKey = gojsKey;
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
